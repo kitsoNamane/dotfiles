dff510f35a06dd009ab7e03764cec27b5ccf16ad
kitsoNamane <kitsogideonnamane@gmail.com>
2021-03-16 04:57:42 +0200

    initialize plugins: install packer and other other plugins

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+-- General Mappings
+require("plugins")     -- lua/plugins.lua
+require('keymappings') -- lua/keymappings.lua
+require('settings')    -- lua/settings.lua
+require('colorscheme') -- lua/colorscheme.lua
+
+
+-- Plugins
+
+
+-- LSP
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,70 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself as an optional plugin
+  use {'wbthomason/packer.nvim'}
+
+  --  LSP
+  use 'neovim/nvim-lspconfig'
+  use 'glepnir/lspsaga.nvim'
+  use 'onsails/lspkind-nvim'
+  use 'kosayoda/nvim-lightbulb'
+  use 'akinsho/flutter-tools.nvim'
+
+  -- Autocomplete
+  use 'hrsh7th/nvim-compe'
+
+  -- Snippets
+  use 'SirVer/ultisnips'
+  use 'hrsh7th/vim-vsnip'
+
+  --  Debugging
+  use 'mfussenegger/nvim-dap'
+
+  --  Treesitter
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'p00f/nvim-ts-rainbow'
+  --
+  --  Icons
+  use 'kyazdani42/nvim-web-devicons'
+
+
+ -- Status Line and Bufferline
+  use 'glepnir/galaxyline.nvim'
+  -- use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
+  use 'romgrk/barbar.nvim'
+  use 'ojroques/nvim-bufdel'
+
+  -- Telescope
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  -- Explorer
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Color
+  use 'christianchiarulli/nvcode-color-schemes.vim'
+  use 'norcalli/nvim-colorizer.lua'
+
+  -- Git
+  use 'TimUntersberger/neogit'
+  use {'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+
+
+  -- General Plugins
+  use 'b3nj5m1n/kommentary'
+  use 'voldikss/vim-floaterm'
+  use 'windwp/nvim-autopairs'
+  use 'kevinhwang91/nvim-bqf'
+  use 'psliwka/vim-smoothie'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-surround'
+end)
